e6986e12e2ed88b2b2f18bda7d68fbbd6381ecc2
malceore <malceore@gmail.com>
2015-12-05 17:10:33 -0500

    Huge cleanup! Changed area to objects.js

--- knave_chat.js
+++
@@ -1,7 +0,0 @@
-
-function runChat(){
-        var stage = new PIXI.Stage(0x50503E);
-        var renderer = PIXI.autoDetectRenderer(622, 280);
-        document.body.appendChild(renderer.view);
-}
-
--- knave_inventory.js
+++
@@ -1,7 +0,0 @@
-function runInventory(){
-        var stage = new PIXI.Stage(0x999966);
-        var renderer = PIXI.autoDetectRenderer(624, 280);
-        document.body.appendChild(renderer.view);
-}
-
-
